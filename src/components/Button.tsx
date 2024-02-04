6605f76e43c978a49b3d030a2a1fce98dcfc7d0f
AnjasFedo <fedoaf98@gmail.com>
2024-02-04 17:33:00 +0700

    Variable & Function Type

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
 
-export default function Button () {
+// let url = "https://www.google.com";
+
+// url = 5;
+
+// console.log(url);
+
+function convertCurrency(amount: number, currency: string): number {
+  console.log(currency);
+  return amount;
+}
+
+console.log(convertCurrency(100, "USD"));
+
+export default function Button() {
   return (
-    <button className='bg-blue-600 text-white rounded-md py-2 px-1' type="button">Button</button>
-  )
+    <button
+      className="bg-blue-600 text-white rounded-md py-2 px-1"
+      type="button"
+    >
+      Button
+    </button>
+  );
 }
