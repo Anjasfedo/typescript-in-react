5520da5cf91a8a3bf861469652e52648fa6f62c0
Anjasfedo <fedoaf98@gmail.com>
2024-02-16 00:08:22 +0700

    Third-party Types

--- src/components/Button_unknown_type.tsx
+++ src/components/Button_unknown_type.tsx
@@ -1,12 +1,15 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
+
+type ButtonProps = {
+  children: React.ReactNode;
+  style: React.CSSProperties;
+};
 
 export default function Button() {
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/todos/1")
       .then((res) => res.json())
-      .then((data: unknown) => {
-        data.name.toUpperCase()
-      });
+      .then((data: unknown) => console.log(data));
   }, []);
 
   return <button>Click Me</button>;
